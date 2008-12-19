675f2f2e3ceec4968ecbf45501f2f1148f0f0466
jakob <jakob@20b5617b-3fbf-4bc0-ad7a-7920de48818a>
2008-12-19 22:06:34 +0000

    - Added license header
    git-svn-id: svn://localhost/evolving_vectorization@10 20b5617b-3fbf-4bc0-ad7a-7920de48818a

--- trunk/src/evolver.c
+++ trunk/src/evolver.c
@@ -1,3 +1,22 @@
+/* 
+ * This file is part of Evolving vectorization
+ * Copyright (C) 2008  Jakob Westhoff
+ * 
+ * Evolving vectorization is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License. 
+ * 
+ * Evolving vectorization is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * arbit; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA  02110-1301  USA 
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>

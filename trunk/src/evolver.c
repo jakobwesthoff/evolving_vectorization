19942e16630f486a2bf22b2e8ba7922a80b7e7ae
jakob <jakob@20b5617b-3fbf-4bc0-ad7a-7920de48818a>
2008-12-19 20:57:40 +0000

    - Made all the different settings configurable
    git-svn-id: svn://localhost/evolving_vectorization@7 20b5617b-3fbf-4bc0-ad7a-7920de48818a

--- trunk/src/evolver.c
+++ trunk/src/evolver.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <math.h>
 
@@ -8,9 +9,253 @@
 #include "random.h"
 #include "polygon.h"
 
-#define INITIAL_TEMPERATURE 1000.0
-#define EPSILON 0.00001
-#define ALPHA 0.99999
+
+static unsigned int quadratic_error( cairo_surface_t* original, cairo_surface_t* destination );
+static void initialize_new_render_surface( cairo_surface_t* input, cairo_surface_t** render_surface );
+static void show_usage();
+
+
+
+int main( int argc, char** argv ) 
+{
+    // Drawing and original cairo surfaces
+    cairo_surface_t* input_surface  = NULL;
+    cairo_surface_t* render_surface = NULL;
+
+    // Polygon structs
+    polygons_t* polygons      = NULL;
+    polygons_t* best_polygons = NULL;
+
+    // Iteration counters
+    unsigned int iteration  = 0;
+    unsigned int benefitial = 0;
+
+    // Fitness levels
+    unsigned int current_fitness = 0xffffffff;
+    unsigned int best_fitness    = 0xffffffff;
+
+    // Default simulated annealing values
+    double temperature = 1000.0;
+    double alpha       = 0.99999;
+    double epsilon     = 0.01;
+
+    // Default writeout values
+    int svg_write_iterations = 10000;
+    int png_write_iterations = 1000;
+
+    // Input file and output directory
+    char* input_file;
+    char* output_directory;
+
+    // Read commandline arguments
+    {
+        extern char *optarg;
+        extern int optind, optopt;
+        int c;
+        while( ( c = getopt( argc, argv, "t:a:e:s:p:" ) ) != -1 ) 
+        {
+            switch( c ) 
+            {
+                case '?':
+                    show_usage();
+                    exit( EXIT_FAILURE );
+                break;
+                case 'p':
+                    png_write_iterations = atoi( optarg );
+                    printf( "png_write_iterations=%u\n", png_write_iterations );
+                break;
+                case 's':
+                    svg_write_iterations = atoi( optarg );
+                    printf( "svg_write_iterations=%u\n", svg_write_iterations );
+                break;
+                case 't':
+                    temperature = strtod( optarg, NULL );
+                    printf( "temperature=%f\n", temperature );
+                break;
+                case 'e':
+                    epsilon = strtod( optarg, NULL );
+                    printf( "epsilon=%f\n", epsilon );
+                break;
+                case 'a':
+                    alpha = strtod( optarg, NULL );
+                    printf( "alpha=%f\n", alpha );
+                break;
+            }
+        }
+    }
+    
+    if ( argc - optind < 2 ) 
+    {
+        show_usage();
+        exit( EXIT_FAILURE );
+    }
+
+    input_file       = argv[optind];      
+    output_directory = argv[optind + 1];
+
+    // Seed the random number generator
+    rand_seed();
+
+    // Load the original image for comparison
+    input_surface = cairo_image_surface_create_from_png( input_file );
+    if ( cairo_surface_status( input_surface ) != CAIRO_STATUS_SUCCESS ) 
+    {
+        printf( "Could not create input surface.\n" );
+        exit( EXIT_FAILURE );
+    }    
+        
+    // Create random polygon structure and initialize all needed values
+    polygons = initialize_polygons( input_surface );
+    best_polygons = copy_polygons( polygons );
+    initialize_new_render_surface( input_surface, &render_surface );
+    draw_polygons( render_surface, polygons );
+    current_fitness = quadratic_error( input_surface, render_surface );
+    best_fitness    = current_fitness;
+    printf( "%u/%u/%f (%u)", benefitial, 0, temperature, best_fitness );
+    
+    // Start simulated annealing cycle to try finding the optimal polygon
+    // approximation of the image
+    while( 1 ) 
+    {
+        unsigned int new_fitness;
+        polygons_t* new_polygons;       
+        
+        // Write newline before any other status message
+        if ( ( png_write_iterations != 0 && iteration % png_write_iterations == 0 )
+          || ( svg_write_iterations != 0 && iteration % svg_write_iterations == 0 ) ) 
+        {
+            printf( "\n" );
+        }
+    
+        // Write output png every x evolutions
+        if ( png_write_iterations != 0 && iteration % png_write_iterations == 0 ) 
+        {
+            char* filename = malloc( sizeof( char ) * ( strlen( output_directory ) + 32 ) );
+
+            initialize_new_render_surface( input_surface, &render_surface );
+            draw_polygons( render_surface, best_polygons );
+            
+            sprintf( filename, "%s/%010u.png", output_directory, iteration );
+            cairo_surface_write_to_png( render_surface, filename );
+
+            printf( "PNG: %s written.\n", filename );
+            free( filename );
+        }
+
+        // Write a svg every x iterations
+        if ( svg_write_iterations != 0 && iteration % svg_write_iterations == 0 ) 
+        {
+            char* filename = malloc( sizeof( char ) * ( strlen( output_directory ) + 32 ) );
+            sprintf( filename, "%s/%010u.svg", output_directory, iteration );
+
+            draw_polygons_to_svg( best_polygons, filename );
+
+            printf( "SVG: %s written.\n", filename );
+            free( filename );
+        }
+
+        // Create new evolution
+        initialize_new_render_surface( input_surface, &render_surface );
+        new_polygons = copy_polygons( polygons );
+        evolve_polygons( new_polygons );
+        draw_polygons( render_surface, new_polygons );
+        new_fitness = quadratic_error( input_surface, render_surface );
+    
+        // Store polygons with the best fitness found so far
+        if ( new_fitness < best_fitness ) 
+        {
+            free( best_polygons );
+            best_polygons = copy_polygons( new_polygons );
+            best_fitness = new_fitness;
+        }
+
+        // If the new evolution is better than the old one the old one will die
+        // and the new one will be taken further
+        if ( new_fitness < current_fitness ) 
+        {
+            ++benefitial;
+            free( polygons );
+            polygons = new_polygons;
+            current_fitness = new_fitness;
+        }
+        else 
+        {
+            // Only change to worse evolution with falling probability based on
+            // the iteration and the fitness difference
+            // Small changes in fitness are more likely to be accepted.
+            if( rand_double() < exp( -1 * ( (double)( new_fitness - current_fitness ) / temperature ) ) ) 
+            {
+                free( polygons );
+                polygons = new_polygons;
+                current_fitness = new_fitness;
+            }
+            else 
+            {
+                // Don't accept the new change
+                free( new_polygons );
+            }
+        }
+
+        printf( "\r%u/%u/%f (%u)            ", benefitial, iteration, temperature, best_fitness );
+
+        // Check for abort condition
+        if( temperature < epsilon ) 
+        {
+            break;
+        }
+
+        // Lower the temperature
+        temperature *= alpha;
+
+        ++iteration;
+    }
+    printf( "\n" );
+
+    // Render the best state found so far to png and svg
+    {
+        char* filename = malloc( sizeof( char ) * strlen( output_directory ) + 16 );
+
+        initialize_new_render_surface( input_surface, &render_surface );
+        draw_polygons( render_surface, best_polygons );
+        sprintf( filename, "%s/final.png", output_directory );
+        cairo_surface_write_to_png( render_surface, filename );
+        printf( "PNG: %s written.\n", filename );
+
+        sprintf( filename, "%s/final.svg", output_directory );
+        draw_polygons_to_svg( best_polygons, filename );
+        printf( "SVG: %s written.\n", filename );
+
+        free( filename );        
+    }
+    
+    // Free the allocated memory
+    if ( polygons != NULL  )
+        free( polygons );
+    if ( best_polygons != NULL  )
+        free( best_polygons );
+
+    cairo_surface_destroy( render_surface );
+    cairo_surface_destroy( input_surface );
+}
+
+static void show_usage() 
+{
+    printf( "Evolving Vectorizer\n" );
+    printf( "(c) Jakob Westhoff <jakob@php.net>\n" );
+    printf( "Usage:\n" );
+    printf( "   evolver [options] <inputfile> <outputdirectory>\n" );
+    printf( "Options:\n" );
+    printf( "   -p <int>:   Save current state as png every <number>\n\
+               iterations (Default: 1000) (0 to disable)\n" );
+    printf( "   -s <int>:   Save current state as svg every <number>\n\
+               iterations (Default: 10000) (0 to disable)\n" );        
+    printf( "   -t <float>: Initial temperature for simulated\n\
+               annealing (Default: 1000.0)\n" );
+    printf( "   -e <float>: Epsilon value to stop the annealing at\n\
+               (Default: 0.01)\n" );
+    printf( "   -a <float>: Alpha to cool temperature by every\n\
+               iteration (Default: 0.99999)\n" );
+}
 
 static unsigned int quadratic_error( cairo_surface_t* original, cairo_surface_t* destination ) 
 {
@@ -48,7 +293,7 @@ static unsigned int quadratic_error( cairo_surface_t* original, cairo_surface_t*
     return quadratic_error;
 }
 
-void initialize_new_render_surface( cairo_surface_t* input, cairo_surface_t** render_surface ) 
+static void initialize_new_render_surface( cairo_surface_t* input, cairo_surface_t** render_surface ) 
 {
     if ( *render_surface != NULL ) 
     {
@@ -67,147 +312,4 @@ void initialize_new_render_surface( cairo_surface_t* input, cairo_surface_t** re
         printf( "Could not create render surface.\n" );
         exit( EXIT_FAILURE );
     }
-}
-
-int main( int argc, char** argv ) 
-{
-    cairo_surface_t* input_surface  = NULL;
-    cairo_surface_t* render_surface = NULL;
-    polygons_t* polygons      = NULL;
-    polygons_t* best_polygons = NULL;
-    unsigned int iteration  = 0;
-    unsigned int benefitial = 0;
-    unsigned int current_fitness = 0xffffffff;
-    unsigned int best_fitness    = 0xffffffff;
-    double temperature = INITIAL_TEMPERATURE;
-    double alpha       = ALPHA;
-    double epsilon     = EPSILON;
-
-    if ( argc < 3 ) 
-    {
-        printf( "Usage:\n" );
-        printf( "   evolver <inputfile> <outputdirectory>\n" );
-        exit( EXIT_FAILURE );
-    }
-
-    // Seed the random number generator
-    rand_seed();
-
-    // Load the original image for comparison
-    input_surface = cairo_image_surface_create_from_png( argv[1] );
-    if ( cairo_surface_status( input_surface ) != CAIRO_STATUS_SUCCESS ) 
-    {
-        printf( "Could not create input surface.\n" );
-        exit( EXIT_FAILURE );
-    }    
-        
-    polygons = initialize_polygons( input_surface );
-    best_polygons = copy_polygons( polygons );
-    initialize_new_render_surface( input_surface, &render_surface );
-    draw_polygons( render_surface, polygons );
-    current_fitness = quadratic_error( input_surface, render_surface );
-    best_fitness    = current_fitness;
-    printf( "%u/%u/%f (%u)", benefitial, 0, temperature, best_fitness );
-    
-    while( 1 ) 
-    {
-        unsigned int new_fitness;
-        polygons_t* new_polygons;       
-
-        // Write output images every 1000 evolutions
-        if ( iteration % 1000 == 0 ) 
-        {
-            char* filename;
-
-            initialize_new_render_surface( input_surface, &render_surface );
-            draw_polygons( render_surface, best_polygons );
-            
-            filename = malloc( sizeof( char ) * ( strlen( argv[2] ) + 32 ) );
-            sprintf( filename, "%s/%010u.png", argv[2], iteration );
-            cairo_surface_write_to_png( render_surface, filename );
-
-            printf( "\nPNG: %s written.\n", filename );
-            free( filename );
-        }
-
-        // Write a svg every 10000 iterations
-        if ( iteration % 10000 == 0 ) 
-        {
-            char* filename = malloc( sizeof( char ) * ( strlen( argv[2] ) + 32 ) );
-            sprintf( filename, "%s/%010u.svg", argv[2], iteration );
-
-            draw_polygons_to_svg( best_polygons, filename );
-
-            printf( "SVG: %s written.\n", filename );
-            free( filename );
-        }
-
-        // Create new evolution
-        initialize_new_render_surface( input_surface, &render_surface );
-        new_polygons = copy_polygons( polygons );
-        evolve_polygons( new_polygons );
-        draw_polygons( render_surface, new_polygons );
-        new_fitness = quadratic_error( input_surface, render_surface );
-    
-        if ( new_fitness < best_fitness ) 
-        {
-            free( best_polygons );
-            best_polygons = copy_polygons( new_polygons );
-            best_fitness = new_fitness;
-        }
-
-        if ( new_fitness < current_fitness ) 
-        {
-            ++benefitial;
-            free( polygons );
-            polygons = new_polygons;
-            current_fitness = new_fitness;
-        }
-        else 
-        {
-            // Only change to worse evolution with falling probability based on
-            // the iteration and the fitness difference
-            // Small changes in fitness are more likely to be accepted.
-            if( rand_double() < exp( -1 * ( (double)( new_fitness - current_fitness ) / temperature ) ) ) 
-            {
-                free( polygons );
-                polygons = new_polygons;
-                current_fitness = new_fitness;
-            }
-            else 
-            {
-                // Don't accept the new change
-                free( new_polygons );
-            }
-        }
-
-        printf( "\r%u/%u/%f (%u)                ", benefitial, iteration, temperature, best_fitness );
-
-        // Check for abort condition
-        if( temperature < epsilon ) 
-        {
-            break;
-        }
-
-        // Lower the temperature
-        temperature *= alpha;
-
-        ++iteration;
-    }
-    printf( "\n" );
-
-    initialize_new_render_surface( input_surface, &render_surface );
-    draw_polygons( render_surface, best_polygons );
-    cairo_surface_write_to_png( render_surface, argv[2] );
-
-    printf( "%s written.\n", argv[2] );
-    
-    // Free the allocated memory
-    if ( polygons != NULL  )
-        free( polygons );
-    if ( best_polygons != NULL  )
-        free( best_polygons );
-
-    cairo_surface_destroy( render_surface );
-    cairo_surface_destroy( input_surface );
 }
